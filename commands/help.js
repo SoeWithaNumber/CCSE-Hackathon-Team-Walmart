1b61a958d8a846a989c2f63e8c4a0460fa04cc93
Tyler <tysisson@comcast.net>
2022-09-16 15:12:27 -0400

    Added base help comand

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+function execute(interaction) {
+    const helpEmbed = new EmbedBuilder()
+        .setTitle("Welcome to Discord Notes!")
+        .setColor("Blue")
+        .setDescription("WIP")
+        .setFooter({ text: "Version 0.1.0-dev" })
+    interaction.reply({ embeds: [helpEmbed] })
+}
+
+module.exports = {
+    execute,
+    data: new SlashCommandBuilder()
+        .setName("help")
+        .setDescription("Discord notes Help Command"),
+    name: "help"
+
+}
+
