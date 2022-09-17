1e333d9adfb4a0b34d083f45b782e152fedba6da
alucyrs <alucardtepes1646@gmail.com>
2022-09-17 09:58:06 -0400

    Created end command for collector

--- commands/subcommands/notes/end.js
+++ commands/subcommands/notes/end.js
@@ -2,7 +2,7 @@ const {} = require("discord.js");
 function execute(interaction) {
     USER_ID = interaction.user.id
     interaction.client.collectors ["USER_ID"].on("end", collected => {
-        interaction.reply("Stopped listening")
+        interaction.reply("Stopped recording notes")
     })
 }
 
