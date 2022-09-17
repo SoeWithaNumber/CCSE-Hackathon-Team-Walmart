84f5aabd93b9d37496d3ca5214c676adabeb5ad2
alucyrs <alucardtepes1646@gmail.com>
2022-09-17 09:53:36 -0400

    Created the connector end

--- commands/subcommands/notes/end.js
+++ commands/subcommands/notes/end.js
@@ -1,6 +1,9 @@
 const {} = require("discord.js");
 function execute(interaction) {
-    console.log("end works")
+    USER_ID = interaction.user.id
+    interaction.client.collectors ["USER_ID"].on("end", collected => {
+        interaction.reply("Stopped listening")
+    })
 }
 
 module.exports = {
